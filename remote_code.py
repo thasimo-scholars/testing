277a23155dd6e6523ec9478be8976618e9479925
thasimo-scholars <99335441+thasimo-scholars@users.noreply.github.com>
2023-04-09 19:13:15 +0200

    Update remote_code.py

--- remote_code.py
+++ remote_code.py
@@ -1,33 +1,15 @@
-# importing whole module
-from tkinter import *
-from tkinter.ttk import *
-
-# importing strftime function to
-# retrieve system's time
-from time import strftime
-
-# creating tkinter window
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
+        
+        text = Label(self, text="Just do it")
+        text.place(x=70,y=90)
+        #text.pack()
+        
 root = Tk()
-root.title('Clock')
-
-# This function is used to
-# display time on the label
-
-def time():
-	string = strftime('%H:%M:%S %p')
-	lbl.config(text=string)
-	lbl.after(1000, time)
-
-
-# Styling the label widget so that clock
-# will look more attractive
-lbl = Label(root, font=('calibri', 40, 'bold'),
-			background='purple',
-			foreground='white')
-
-# Placing clock at the centre
-# of the tkinter window
-lbl.pack(anchor='center')
-time()
-
-mainloop()
+app = Window(root)
+root.wm_title("Tkinter window")
+root.geometry("200x200")
+root.mainloop()
