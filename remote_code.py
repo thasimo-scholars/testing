42e089d23fc267001e2a92fa4402028d392a3454
thasimo-scholars <99335441+thasimo-scholars@users.noreply.github.com>
2023-04-09 19:51:33 +0200

    Update remote_code.py

--- remote_code.py
+++ remote_code.py
@@ -1,4 +1,1 @@
-def show_message_box(title, message):
-    ctypes.windll.user32.MessageBoxW(0, message, title, 0x40 | 0x1)
-
-show_message_box("Title", "This is a message.")
+ctypes.windll.user32.MessageBoxW(0, "This is a message.", "Title", 0x40 | 0x1)
